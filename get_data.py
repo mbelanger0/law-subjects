37f9e0b4d2f98bc66c992eb592494a07eb686f1b
Mark Belanger <mbelanger@olin.edu>
2023-03-22 13:20:26 -0400

    Use Pandas to write data to csv

--- get_data.py
+++ get_data.py
@@ -1,29 +1,34 @@
 import requests, json
+import pandas as pd
 
 
 def get_data(start_congress, end_congress, API_KEY):
     """
     this is a docstring.
     """
-    with open("bill_data.csv", "w") as bill_data:
-        for congress in range(start_congress, end_congress + 1):
-            bills = requests.get(
-                f"https://api.congress.gov/v3/bill/{congress}/s?&api_key={API_KEY}"
+    all_data = []
+    for congress in range(start_congress, end_congress + 1):
+        bills = requests.get(
+            f"https://api.congress.gov/v3/bill/{congress}/s?&api_key={API_KEY}"
+        )
+        bill_nye = bills.json()
+        bills_list = bill_nye["bills"]
+        for bill in bills_list:
+            bill_number = bill["number"]
+            individual_bill = requests.get(
+                f"https://api.congress.gov/v3/bill/{congress}/s/{bill_number}?api_key={API_KEY}"
             )
-            bill_nye = bills.json()
-            bill_values = []
-            bills_list = bill_nye["bills"]
-            for bill in bills_list:
-                bill_number = bill["number"]
-                individual_bill = requests.get(
-                    f"https://api.congress.gov/v3/bill/{congress}/s/{bill_number}?api_key={API_KEY}"
-                )
-                bill_dict = individual_bill.json()["bill"]
-                for sponsor in bill_dict["sponsors"]:
-                    bill_values = [
+            bill_dict = individual_bill.json()["bill"]
+            for sponsor in bill_dict["sponsors"]:
+                all_data.append(
+                    [
                         bill_dict["number"],
                         sponsor["fullName"],
                         sponsor["state"],
                         bill_dict["policyArea"]["name"],
                     ]
-                    print(bill_values, file=bill_data)
+                )
+    bills_dataframe = pd.DataFrame(all_data)
+    bills_dataframe.columns = ["Number", "Senator", "State", "Policy Areas"]
+    bills_dataframe.to_csv("bills_data.csv", index=False)
+    pd.read_csv("bills_data.csv", names=["Number", "Senator", "State", "Policy Areas"])
