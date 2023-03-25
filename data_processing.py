e8c9ca29767be2f39dff6eb68cb1466ef7a218c3
Noah Rand <nrand@olin.edu>
2023-03-25 17:07:30 -0400

    Create data processing functions and essay file.

---
+++ data_processing.py
@@ -0,0 +1,69 @@
+""" A file containing the data processing functions."""
+
+subject_list = [
+    "Agriculture and Food",
+    "Animals",
+    "Armed Forces and National Security",
+    "Arts, Culture, Religion",
+    "Civil Rights and Liberties, Minority Issues",
+    "Commerce",
+    "Congress",
+    "Crime and Law Enforcement",
+    "Economics and Public Finance",
+    "Education",
+    "Emergency Management",
+    "Energy",
+    "Environmental Protection",
+    "Families",
+    "Finance and Financial Sector",
+    "Foreign Trade and International Finance",
+    "Government Operations and Politics",
+    "Health",
+    "Housing and Community Development",
+    "Immigration",
+    "International Affairs",
+    "Labor and Employment",
+    "Law",
+    "Native Americans",
+    "Public Lands and Natural Resources",
+    "Science, Technology, Communications",
+    "Social Sciences and History",
+    "Social Welfare",
+    "Sports and Recreation",
+    "Taxation",
+    "Transportation and Public Works",
+    "Water Resources Development",
+]
+
+
+def count_subject(state_policy_dict):
+    """
+    Counts the number of times each state presents bills with each
+    policy subject
+
+    Args:
+        state_policy_dict: a dictionary where the keys are each state
+            that proposed legislation, and the values are lists containing
+            each category of legislation.
+
+    Returns:
+        state_subject_dict: a dictionary where the keys are each state, and the
+            values are dictionaries where each each key is a law subject and
+            each value is the number of times the state passed legislation of that
+            subject.
+    """
+    state_subject_dict = {}
+    for state in state_policy_dict:
+        state_subject_dict[state] = {}
+        for subject in subject_list:
+            subject_count = state_policy_dict[state].count(subject)
+            state_subject_dict[state][subject] = subject_count
+    return state_subject_dict
+
+
+def popular_subject(state_subject_dict):
+    """
+    Returns each state's most popular legislation subject. If there are multiple
+    equally preferred subjects, returns the alphabetically first
+    (besed on subject_list)
+    """
