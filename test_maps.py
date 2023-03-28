ef819764499b85d789e641d6a598232d9e365519
Noah Rand <nrand@olin.edu>
2023-03-28 11:40:53 -0400

    Add test_add_map_data_three_dictionaries unit test

--- test_maps.py
+++ test_maps.py
@@ -1,4 +1,20 @@
+""" 
+Test files for getting and manipulating data for creating geographical plots
+from maps.py. 
+"""
 import maps
+
+def test_add_map_data_three_dictionaries():
+    """
+    Test that the function combines dictionaries as expected
+    """
+    group = {"features": [{"properties":{"NH": "Mark", "MO": "Noah", "CO": "Crazy Noah"}]
+    professor = {"Steve": "Some Professor", "Aman": "Another Professor", "Erhardt": "My Professor"}
+    course = {"QEA": 100, "SoftDes": 1020, "ISIM": 830}
+
+    assert maps.add_map_data(group, professor, course) == {
+        {"NH": "Mark", "Steve": "Some Professor", "QEA": 100}
+    }
 
 
 def test_make_geodata_datatype():
